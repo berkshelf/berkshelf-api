d9bb4660f8bb4e5c72ae43de42d9e651ee185a41
Jamie Winsor <jamie@vialstudios.com>
2013-06-12 18:00:08 -0700

    add option parsing to binary and application supervisor

--- spec/unit/berkshelf/api/application_spec.rb
+++ spec/unit/berkshelf/api/application_spec.rb
@@ -5,5 +5,23 @@ describe Berkshelf::API::Application do
     subject { described_class }
 
     its(:registry) { should be_a(Celluloid::Registry) }
+
+    describe "::parse_options" do
+      let(:arguments) { Array.new }
+      subject { described_class.parse_options(arguments) }
+
+      it "returns a Hash" do
+        expect(subject).to be_a(Hash)
+      end
+
+      context "given a value for -p" do
+        let(:arguments) { ["-p", "1984"] }
+
+        it "assigns the value as an integer to :port" do
+          expect(subject).to have_key(:port)
+          expect(subject[:port]).to eql(1984)
+        end
+      end
+    end
   end
 end
