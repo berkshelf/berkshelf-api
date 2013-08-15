6350ca4bc3048c4f89741ef631309dd6acee5251
Andrew Garson <andrew.garson@gmail.com>
2013-08-15 16:54:10 -0700

    Remove a bug fix that shouldn't be on this branch

--- lib/berkshelf/api/remote_cookbook.rb
+++ lib/berkshelf/api/remote_cookbook.rb
@@ -1,11 +1,3 @@
 module Berkshelf::API
   class RemoteCookbook < Struct.new(:name, :version, :location_type, :location_path)
-    def hash
-      "#{name}|#{version}".hash
-    end
-
-    def eql?(other)
-      self.hash == other.hash
-    end
-  end
 end
