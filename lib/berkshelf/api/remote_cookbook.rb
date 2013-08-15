e343d0afc3b74a98b4d3f254df6c607c11e91b52
Andrew Garson <andrew.garson@gmail.com>
2013-08-15 16:55:30 -0700

    RemoteCookbooks are compared using set notation, this requires you to implement #eql? and #hash for appropriate behavior
    Without this, if you save a full cache then restart the server, it will
    rebuild the cache as if it were empty (removing all existing items on
    the first pass through).

--- lib/berkshelf/api/remote_cookbook.rb
+++ lib/berkshelf/api/remote_cookbook.rb
@@ -1,3 +1,12 @@
 module Berkshelf::API
   class RemoteCookbook < Struct.new(:name, :version, :location_type, :location_path); end
+
+  def hash
+    "#{name}|#{version}".hash
+  end
+
+  def eql?(other)
+    self.hash == other.hash
+  end
+
 end
