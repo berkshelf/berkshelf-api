9f588cfb2726f635dceb8c41dcce8c1c06d4b081
Andrew Garson <andrew.garson@gmail.com>
2013-08-15 17:00:17 -0700

    syntax error

--- lib/berkshelf/api/remote_cookbook.rb
+++ lib/berkshelf/api/remote_cookbook.rb
@@ -1,3 +1,3 @@
 module Berkshelf::API
-  class RemoteCookbook < Struct.new(:name, :version, :location_type, :location_path)
+  class RemoteCookbook < Struct.new(:name, :version, :location_type, :location_path); end
 end
