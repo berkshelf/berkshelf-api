1dbb8c863baf8e5ba2f54eccb56954d260bc41fd
Brian Fletcher <brian.jf.fletcher@gmail.com>
2013-12-06 01:03:35 +0000

    adds a github cache builder worker

---
+++ lib/berkshelf/api/cache_builder/worker/github.rb
@@ -0,0 +1,63 @@
+require 'octokit'
+require 'solve'
+require 'net/http'
+
+module Berkshelf::API
+  class CacheBuilder
+    module Worker
+      class Github < Worker::Base
+        worker_type "github"
+
+        def initialize(options = {})
+          @connection = Octokit::Client.new :access_token => options[:access_token]
+          @organization = options[:organization]
+          super
+        end
+
+        # @return [Array<RemoteCookbook>]
+        #  The list of cookbooks this builder can find
+        def cookbooks
+          [].tap do |cookbook_versions|
+            @connection.organization_repositories(@organization).each do |repo|
+              @connection.tags(repo.full_name).each do |tag|
+                if match = /^v(?<version>.*)$/.match(tag.name)
+                  begin
+                    version = Solve::Version.new(match[:version])
+                    cookbook_metadata = Ridley::Chef::Cookbook::Metadata.new
+                    metadata_content = Base64.decode64(@connection.contents(repo.full_name, :path => 'metadata.rb', :ref => tag.name).content)
+                    cookbook_metadata.instance_eval(metadata_content)
+                    if cookbook_metadata.version == match[:version]
+                      cookbook_versions << RemoteCookbook.new(repo.name, cookbook_metadata.version, self.class.worker_type, repo.full_name, priority)
+                      log.debug "Repository #{repo.name} looks like it has a good tag name, #{tag.name}, #{cookbook_metadata.version}"
+                    else
+                      log.warn "There is a conflicting tag name for: #{repo.name}, Tag: #{tag.name}, does not match the metadata version #{cookbook_metadata.version}"
+                    end
+                  rescue Solve::Errors::InvalidVersionFormat => e
+                    log.debug "Ignoring tag #{tag.name} for: #{repo.name} as it doesn not conform to semver"
+                  rescue Octokit::NotFound => e
+                    log.debug "Ignoring tag #{tag.name} for: #{repo.name} as it doesnt seem to have a metadata.rb"
+                  end
+                else
+                  log.debug 'Version number cannot be parsed'
+                end
+              end
+            end
+          end
+        end
+
+        # Return the metadata of the given RemoteCookbook. If the metadata could not be found or parsed
+        # nil is returned.
+        #
+        # @param [RemoteCookbook] remote
+        #
+        # @return [Ridley::Chef::Cookbook::Metadata, nil]
+        def metadata(remote)
+          cookbook_metadata = Ridley::Chef::Cookbook::Metadata.new
+          metadata_content = Base64.decode64(@connection.contents("#{@organization}/#{remote.name}", :path => 'metadata.rb', :ref => "v#{remote.version}").content)
+          cookbook_metadata.instance_eval(metadata_content)
+          return cookbook_metadata
+        end
+      end
+    end
+  end
+end
---
+++ spec/unit/berkshelf/api/cache_builder/worker/github_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+
+describe Berkshelf::API::CacheBuilder::Worker::Github do
+  describe "ClassMethods" do
+    subject { described_class }
+    its(:worker_type) { should eql("github") }
+  end
+
+  let(:connection) do
+    double('connection')
+  end
+
+  let(:good_tag) do
+    tag = double('good_tag')
+    tag.stub(:name) { 'v1.0.0' }
+    tag
+  end
+
+  let(:bad_tag) do
+    tag = double('good_tag')
+    tag.stub(:name) { 'beta2' }
+    tag
+  end
+
+  let :contents do
+    contents = double('contents')
+    contents.stub(:content) { 'dmVyc2lvbiAiMS4wLjAi' }
+    contents
+  end
+
+  let(:repo) do
+    repo = double('repo')
+    repo.stub(:full_name) { 'opscode-cookbooks/apt' }
+    repo.stub(:name) { 'apt' }
+    repo
+  end
+
+  let(:repos) do
+    [repo]
+  end
+
+  subject do
+    Octokit::Client = double("Octokit::Client")
+    expect(Octokit::Client).to receive(:new) { connection }
+    described_class.new(organization: "opscode-cookbooks",
+        access_token: "asdf")
+  end
+
+  describe "#cookbooks" do
+    before do
+      expect(connection).to receive(:organization_repositories) { repos }
+      expect(connection).to receive(:tags) { [good_tag, bad_tag] }
+      expect(connection).to receive(:contents).with("opscode-cookbooks/apt", {:path=>"metadata.rb", :ref=>"v1.0.0"}) { contents }
+    end
+
+    it "returns an array containing an item for each valid cookbook on the server" do
+      expect(subject.cookbooks).to have(1).items
+    end
+
+    it "returns an array of RemoteCookbooks" do
+      subject.cookbooks.each do |cookbook|
+        expect(cookbook).to be_a(Berkshelf::API::RemoteCookbook)
+      end
+    end
+
+    it "each RemoteCookbook is tagged with a location_type matching the worker_type of the builder" do
+      subject.cookbooks.each do |cookbook|
+        expect(cookbook.location_type).to eql(described_class.worker_type)
+      end
+    end
+  end
+end
