c378c51a739d362d29ed70aaa1f52db1b75a2c90
Noah Kantrowitz <noah@coderanger.net>
2014-04-29 12:28:01 -0700

    Return JSON to naive HTTP clients like browsers or curl.

--- lib/berkshelf/api/endpoint.rb
+++ lib/berkshelf/api/endpoint.rb
@@ -7,6 +7,8 @@ module Berkshelf::API
       # Force inbound requests to be JSON
       def call(env)
         env['CONTENT_TYPE'] = 'application/json'
+        # If coming from a browser or other naive HTTP client, we want JSON back
+        env['HTTP_ACCEPT'] = 'application/json' if !env['HTTP_ACCEPT'] || env['HTTP_ACCEPT'].include?('text/html')
         super
       end
     end
