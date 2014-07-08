a4a02b0fca9e09c08809a3744f3339d3fb4b2d05
Seth Vargo <sethvargo@gmail.com>
2014-07-08 16:25:45 -0400

    Use > instead of greater_than

--- spec/unit/berkshelf/api/rack_app_spec.rb
+++ spec/unit/berkshelf/api/rack_app_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Berkshelf::API::RackApp do
   describe '.endpoints' do
     it 'has at least one item' do
-      expect(described_class.endpoints.size).to be_greater_than(0)
+      expect(described_class.endpoints.size).to be > 0
     end
   end
 end
