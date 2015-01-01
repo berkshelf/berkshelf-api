396386786c43971eb6e824d75536b43ef58a6fbb
Kaito Udagawa <umireon@gmail.com>
2015-01-01 15:20:26 +0900

    Cookbook metadata should not depend on the berkshelf gem Because a chef distribution does not necessarily contain the berkshelf gem. At least, the chef-client 12.0.3 does not include the berkshelf gem And the chef client local mode execution fails with a cryptic error message.

--- cookbook/metadata.rb
+++ cookbook/metadata.rb
@@ -1,14 +1,10 @@
-lib = File.expand_path('../../lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'berkshelf/api/version'
-
 name             "berkshelf-api-server"
 maintainer       "Jamie Winsor"
 maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "Installs/Configures a berkshelf-api server"
 long_description "Installs/Configures a berkshelf-api server"
-version          Berkshelf::API::VERSION
+version          "2.1.0"
 
 %w{ redhat centos ubuntu }.each do |os|
   supports os
