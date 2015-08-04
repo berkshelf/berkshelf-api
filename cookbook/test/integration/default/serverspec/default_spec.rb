2715701e0884bbe5a44697a93f5b1dee14a218af
Dan Webb <dan.webb@capitalone.com>
2015-08-04 08:00:32 +0100

    Update tests and add ubuntu

--- cookbook/test/integration/default/serverspec/default_spec.rb
+++ cookbook/test/integration/default/serverspec/default_spec.rb
@@ -2,8 +2,6 @@ require 'serverspec'
 
 set :backend, :exec
 
-describe 'berkshelf-api-server::default' do
-  service('berks-api') do
-    it { should be_running }
-  end
+describe service('berks-api') do
+  it { should be_running }
 end
